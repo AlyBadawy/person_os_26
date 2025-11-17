fc7985e23dad71479ee2773f76825a1e93054e10
Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>
2025-11-17 10:40:02 -0500

    fix: Improve validation methods by adding early return for blank values

--- app/models/health/body_measurement.rb
+++ app/models/health/body_measurement.rb
@@ -10,12 +10,16 @@ class Health::BodyMeasurement < ApplicationRecord
   private
 
   def data_must_be_valid_json
+    return if data.blank? # handled by presence validation
+
     unless data.is_a?(Hash)
       errors.add(:data, "must be a valid JSON object")
     end
   end
 
   def topic_must_be_valid
+    return if topic.blank? # handled by presence validation
+
     unless HealthMeasurementsTopics.all.include?(topic)
       errors.add(:topic, "must be a valid topic")
     end
